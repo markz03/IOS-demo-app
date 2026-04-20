c7e1e0f124d5e9aa0fa1310b157d324c5a38a5e2
BT_C09_27 <mobileappdev@bths.edu>
2026-04-20 14:33:43 -0400

    Initial Commit

---
+++ ZhouGit/ZhouGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhouGitApp.swift
+//  ZhouGit
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ZhouGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
