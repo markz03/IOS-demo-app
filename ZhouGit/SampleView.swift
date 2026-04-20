840288ef118f9ef628e6b2490ffa7862e4a343dc
BT_C09_27 <mobileappdev@bths.edu>
2026-04-20 14:40:30 -0400

    added the SampleVIew file

---
+++ ZhouGit/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  ZhouGit
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SampleView()
+}
