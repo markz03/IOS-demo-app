0d80b323ef831dcac634094821b9f652f65c7270
BT_C09_27 <mobileappdev@bths.edu>
2026-04-20 14:43:57 -0400

    added a new image to the ContentView file and modified the initial image + deleted the text from SampleView

--- ZhouGit/ContentView.swift
+++ ZhouGit/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "flag")
+            Image(systemName: "book")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Fun with Git.")
+            Image(systemName: "house")
         }
         .padding()
     }
