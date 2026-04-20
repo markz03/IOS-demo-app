cd1eb6f8a2d50ff6ce9b05b2ec57dd6a4e31719c
BT_C09_27 <mobileappdev@bths.edu>
2026-04-20 14:44:36 -0400

    added MyView file

---
+++ ZhouGit/MyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyView.swift
+//  ZhouGit
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyView()
+}
