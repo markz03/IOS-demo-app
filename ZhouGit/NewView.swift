0ff250a7d74dc10743ecb86794a53a7fba653a98
BT_C09_27 <mobileappdev@bths.edu>
2026-04-22 14:21:41 -0400

    added NewView file

---
+++ ZhouGit/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  ZhouGit
+//
+//  Created by Student on 4/22/26.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text("this is a new view")
+    }
+}
+
+#Preview {
+    NewView()
+}
