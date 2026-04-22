9a0d4ddc325b5f127814492b5a31e3c0992c17a9
BT_C09_27 <mobileappdev@bths.edu>
2026-04-22 14:23:08 -0400

    eraser image and text added to NewView

--- ZhouGit/NewView.swift
+++ ZhouGit/NewView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct NewView: View {
     var body: some View {
         Text("this is a new view")
+        Image(systemName: "bolt")
+        Image(systemName: "eraser")
+        Text("erase")
+        
     }
 }
 
